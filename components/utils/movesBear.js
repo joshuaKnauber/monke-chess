5880265a4dc1baeb573222be2b9df039f433ab79
Joshua Knauber <joshua.knauber@gmail.com>
2022-07-05 17:41:59 +0200

    fixed bear

--- components/utils/movesBear.js
+++ components/utils/movesBear.js
@@ -2,9 +2,8 @@ import { removeImpossible } from "./helpers"
 
 export default function movesBear(gameState, piece) {
   if (!piece.isDeployed) {
-    return [
-      [3, 3], [4, 3], [3, 4], [4, 4],
-    ]
+    moves = [[3, 3], [4, 3], [3, 4], [4, 4]]
+    return removeImpossible(gameState, moves, piece)
   }
 
   let moves = [[piece.x + 1, piece.y], [piece.x - 1, piece.y],[piece.x, piece.y + 1],[piece.x + 1, piece.y + 1], [piece.x - 1, piece.y + 1],[piece.x, piece.y - 1], [piece.x + 1, piece.y - 1], [piece.x - 1, piece.y - 1]]
