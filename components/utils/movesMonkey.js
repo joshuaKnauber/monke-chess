7fb5929f0fe922f0f4cae30cc38409d51f427ed1
Joshua Knauber <joshua.knauber@gmail.com>
2022-07-05 17:36:37 +0200

    commented monkey

--- components/utils/movesMonkey.js
+++ components/utils/movesMonkey.js
@@ -14,70 +14,70 @@ export default function movesMonkey(gameState, piece) {
 
 function swing(gamestate, square, foundMoves, finalMoves) {
 
-    if (getPiece(gameState,piece.x - 1, piece.y - 1)) {
-        if (/*[square[0] - 2, square[1] - 2] is not in foundMoves or finalMoves, a piece or outside the boeard*/) {
-            let temp = swing(gamestate, [square[0] - 2, square[1] - 2], foundMoves, finalMoves)
-            foundMoves = foundMoves.concat(temp[0])
-            finalMoves = finalMoves.concat(temp[1])
+    // if (getPiece(gameState,piece.x - 1, piece.y - 1)) {
+    //     if (/*[square[0] - 2, square[1] - 2] is not in foundMoves or finalMoves, a piece or outside the boeard*/) {
+    //         let temp = swing(gamestate, [square[0] - 2, square[1] - 2], foundMoves, finalMoves)
+    //         foundMoves = foundMoves.concat(temp[0])
+    //         finalMoves = finalMoves.concat(temp[1])
             
-        }
-    }
-    if (getPiece(gameState,piece.x - 1, piece.y + 0)) {
-        if (/*[square[0] - 2, square[1] + 0] is not in foundMoves or finalMoves*/) {
-            let temp = swing(gamestate, [square[0] - 2, square[1] + 0], foundMoves, finalMoves)
-            foundMoves = foundMoves.concat(temp[0])
-            finalMoves = finalMoves.concat(temp[1])
+    //     }
+    // }
+    // if (getPiece(gameState,piece.x - 1, piece.y + 0)) {
+    //     if (/*[square[0] - 2, square[1] + 0] is not in foundMoves or finalMoves*/) {
+    //         let temp = swing(gamestate, [square[0] - 2, square[1] + 0], foundMoves, finalMoves)
+    //         foundMoves = foundMoves.concat(temp[0])
+    //         finalMoves = finalMoves.concat(temp[1])
             
-        }
-    }
-    if (getPiece(gameState,piece.x - 1, piece.y + 1)) {
-        if (/*[square[0] - 2, square[1] + 2] is not in foundMoves or finalMoves*/) {
-            let temp = swing(gamestate, [square[0] - 2, square[1] + 2], foundMoves, finalMoves)
-            foundMoves = foundMoves.concat(temp[0])
-            finalMoves = finalMoves.concat(temp[1])
+    //     }
+    // }
+    // if (getPiece(gameState,piece.x - 1, piece.y + 1)) {
+    //     if (/*[square[0] - 2, square[1] + 2] is not in foundMoves or finalMoves*/) {
+    //         let temp = swing(gamestate, [square[0] - 2, square[1] + 2], foundMoves, finalMoves)
+    //         foundMoves = foundMoves.concat(temp[0])
+    //         finalMoves = finalMoves.concat(temp[1])
             
-        }
-    }
-    if (getPiece(gameState,piece.x + 0, piece.y + 1)) {
-        if (/*[square[0] + 0, square[1] + 2] is not in foundMoves or finalMoves*/) {
-            let temp = swing(gamestate, [square[0] + 0, square[1] + 2], foundMoves, finalMoves)
-            foundMoves = foundMoves.concat(temp[0])
-            finalMoves = finalMoves.concat(temp[1])
+    //     }
+    // }
+    // if (getPiece(gameState,piece.x + 0, piece.y + 1)) {
+    //     if (/*[square[0] + 0, square[1] + 2] is not in foundMoves or finalMoves*/) {
+    //         let temp = swing(gamestate, [square[0] + 0, square[1] + 2], foundMoves, finalMoves)
+    //         foundMoves = foundMoves.concat(temp[0])
+    //         finalMoves = finalMoves.concat(temp[1])
             
-        }
-    }
-    if (getPiece(gameState,piece.x + 1, piece.y - 1)) {
-        if (/*[square[0] + 2, square[1] - 2] is not in foundMoves or finalMoves*/) {
-            let temp = swing(gamestate, [square[0] + 2, square[1] - 2], foundMoves, finalMoves)
-            foundMoves = foundMoves.concat(temp[0])
-            finalMoves = finalMoves.concat(temp[1])
+    //     }
+    // }
+    // if (getPiece(gameState,piece.x + 1, piece.y - 1)) {
+    //     if (/*[square[0] + 2, square[1] - 2] is not in foundMoves or finalMoves*/) {
+    //         let temp = swing(gamestate, [square[0] + 2, square[1] - 2], foundMoves, finalMoves)
+    //         foundMoves = foundMoves.concat(temp[0])
+    //         finalMoves = finalMoves.concat(temp[1])
             
-        }
-    }
-    if (getPiece(gameState,piece.x + 1, piece.y + 0)) {
-        if (/*[square[0] + 2, square[1] + 0] is not in foundMoves or finalMoves*/) {
-            let temp = swing(gamestate, [square[0] + 2, square[1] + 0], foundMoves, finalMoves)
-            foundMoves = foundMoves.concat(temp[0])
-            finalMoves = finalMoves.concat(temp[1])
+    //     }
+    // }
+    // if (getPiece(gameState,piece.x + 1, piece.y + 0)) {
+    //     if (/*[square[0] + 2, square[1] + 0] is not in foundMoves or finalMoves*/) {
+    //         let temp = swing(gamestate, [square[0] + 2, square[1] + 0], foundMoves, finalMoves)
+    //         foundMoves = foundMoves.concat(temp[0])
+    //         finalMoves = finalMoves.concat(temp[1])
             
-        }
-    }
-    if (getPiece(gameState,piece.x + 1, piece.y + 1)) {
-        if (/*[square[0] + 2, square[1] + 2] is not in foundMoves or finalMoves*/) {
-            let temp = swing(gamestate, [square[0] + 2, square[1] + 2], foundMoves, finalMoves)
-            foundMoves = foundMoves.concat(temp[0])
-            finalMoves = finalMoves.concat(temp[1])
+    //     }
+    // }
+    // if (getPiece(gameState,piece.x + 1, piece.y + 1)) {
+    //     if (/*[square[0] + 2, square[1] + 2] is not in foundMoves or finalMoves*/) {
+    //         let temp = swing(gamestate, [square[0] + 2, square[1] + 2], foundMoves, finalMoves)
+    //         foundMoves = foundMoves.concat(temp[0])
+    //         finalMoves = finalMoves.concat(temp[1])
             
-        }
-    }
-    if (getPiece(gameState,piece.x + 0, piece.y - 1)) {
-        if (/*[square[0] + 0, square[1] - 2] is not in foundMoves or finalMoves*/) {
-            let temp = swing(gamestate, [square[0] + 0, square[1] - 2], foundMoves, finalMoves)
-            foundMoves = foundMoves.concat(temp[0])
-            finalMoves = finalMoves.concat(temp[1])
+    //     }
+    // }
+    // if (getPiece(gameState,piece.x + 0, piece.y - 1)) {
+    //     if (/*[square[0] + 0, square[1] - 2] is not in foundMoves or finalMoves*/) {
+    //         let temp = swing(gamestate, [square[0] + 0, square[1] - 2], foundMoves, finalMoves)
+    //         foundMoves = foundMoves.concat(temp[0])
+    //         finalMoves = finalMoves.concat(temp[1])
             
-        }
-    }
+    //     }
+    // }
 
     return [foundMoves, finalMoves]
 }
