1c83ddd7846d2e7ced43b058d92133edc6d25155
Joshua Knauber <joshua.knauber@gmail.com>
2022-07-10 00:50:29 +0200

    testing image

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta property="og:image" content="https://upload.wikimedia.org/wikipedia/commons/4/4e/Macaca_nigra_self-portrait_large.jpg" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
