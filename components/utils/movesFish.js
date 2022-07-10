73c583a7dd3c3316483df9a93dfc4305f4c21b64
Joshua Knauber <joshua.knauber@gmail.com>
2022-07-10 12:53:57 +0200

    fixed fish movement

--- components/utils/movesFish.js
+++ components/utils/movesFish.js
@@ -1,5 +1,5 @@
 import movesQueen from "./movesQueen";
-import { removeImpossible } from "./helpers";
+import { getPiece, removeImpossible } from "./helpers";
 
 export default function movesFish(gameState, piece) {
 
@@ -7,15 +7,26 @@ export default function movesFish(gameState, piece) {
     return movesQueen(gameState, piece);
   }
 
-  let moves = [[piece.x + 1, piece.y], [piece.x - 1, piece.y]]
+  let moves = []
+  if (!getPiece(gameState, piece.x + 1, piece.y)) {
+    moves.push([piece.x + 1, piece.y]);
+  } else if (!getPiece(gameState, piece.x - 1, piece.y)) {
+    moves.push([piece.x - 1, piece.y]);
+  }
 
   if (piece.white) {
     if (piece.y < 7) {
-      moves.push([piece.x, piece.y + 1],[piece.x + 1, piece.y + 1], [piece.x - 1, piece.y + 1])
+      moves.push([piece.x + 1, piece.y + 1], [piece.x - 1, piece.y + 1])
+      if (!getPiece(gameState, piece.x, piece.y + 1)) {
+        moves.push([piece.x, piece.y + 1]);
+      }
     }
   } else {
     if (piece.y > 0) {
-      moves.push([piece.x, piece.y - 1], [piece.x + 1, piece.y - 1], [piece.x - 1, piece.y - 1])
+      moves.push([piece.x + 1, piece.y - 1], [piece.x - 1, piece.y - 1])
+      if (!getPiece(gameState, piece.x, piece.y - 1)) {
+        moves.push([piece.x, piece.y - 1]);
+      }
     }
   }
 
